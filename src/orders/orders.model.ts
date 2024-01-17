861cbf16c0dbf0971bd761f3f88b8969e2c77ca7
Nikita Zhuro <nikitazhuro@yandex.by>
2024-01-17 22:32:10 +0300

    fix last

--- src/orders/orders.model.ts
+++ src/orders/orders.model.ts
@@ -5,6 +5,8 @@ export interface IOrder {
   id: number;
   name: string;
   number: string;
+  comment?: string;
+  budget?: string;
 }
 
 @Table({ tableName: 'orders' })
@@ -20,6 +22,12 @@ export class OrdersModel extends Model<OrdersModel, IOrder> {
 
   @Column({ type: DataType.STRING })
   comment: string;
+
+  @Column({ type: DataType.STRING })
+  userComment: string;
+
+  @Column({ type: DataType.STRING })
+  budget: string;
 
   @Column({ type: DataType.BOOLEAN })
   complete: boolean;
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import axios from 'axios';
-import dayjs from 'dayjs';
+import * as dayjs from 'dayjs';
 
 import { OrdersModel } from './orders.model';
 
@@ -10,11 +10,23 @@ export class OrdersService {
   constructor(
     @InjectModel(OrdersModel) private ordersRepository: typeof OrdersModel,
   ) {}
-  async createOrder({ name, number }: { name: string; number: string }) {
+  async createOrder({
+    name,
+    number,
+    userComment,
+    budget,
+  }: {
+    name: string;
+    number: string;
+    userComment: string;
+    budget: string;
+  }) {
     const config = {
       name,
       number,
       comment: '',
+      userComment: userComment || '',
+      budget: budget || '',
       complete: false,
     };
 
@@ -24,7 +36,7 @@ export class OrdersService {
 
     if (!existOrder) {
       await this.ordersRepository.create(config);
-      this.sendMessageToAll({ name, number });
+      this.sendMessageToAll({ name, number, userComment, budget });
     }
   }
   async updateOrder({
@@ -49,25 +61,35 @@ export class OrdersService {
     await order.save();
   }
 
-  async sendMessageToAll({ name, number }) {
+  async sendMessageToAll({ name, number, userComment, budget }) {
     const secret = '6918841607:AAGCWW_MGrx3K_NTN5J3WxrxpYIt3g9rhBg';
     const chatsIds = [1715992777, 323934151, 298938846];
 
-    const fields = [
-      `<b>Имя</b>: ${name}`,
-      `<b>Номер</b>: ${number}`,
-      `<b>Заявка от:</b> ${dayjs().format('DD.MM.YY, HH:mm')}`,
-    ];
+    const fields = [`<b>Имя</b>: ${name}`, `<b>Номер</b>: ${number}`];
+
+    if (budget) {
+      fields.push(`<b>Бюджет</b>: ${budget}`);
+    }
+
+    if (userComment) {
+      fields.push(`<b>Комментарий</b>: ${userComment}`);
+    }
+
+    const date = dayjs().format('DD.MM.YY, HH:mm');
+
+    fields.push(`<b>Заявка от:</b> ${date}`);
+
     let msg = '';
     fields.forEach((field) => {
       msg += field + '\n';
     });
 
+    console.log(msg, 'msg');
+
     msg = encodeURI(msg);
 
     for (let i = 0; i < chatsIds.length; i++) {
       const id = chatsIds[i];
-
       await axios.post(
         `https://api.telegram.org/bot${secret}/sendMessage?chat_id=${id}&parse_mode=html&text=${msg}`,
       );
